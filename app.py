94361023e931ebadfc1baa78efee5b9c3cbb0d29
Leland Tate <lt4753@gmail.com>
2025-07-20 18:18:46 -0400

    main commit

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ def get_eml_file_lines(outlook_filename):
                 if '@' in to:
                     additional_eml_info.append(f'To: {to}')
 
-            if 'Cc:' or 'CC:' in reader:
+            if 'Cc: ' or 'CC: ' in reader:
                 cc_raw = reader['Cc']
                 cc_single_line = re.sub(r'\r?\n[ \t]+', '', cc_raw).split(',')
                 cc_list = ''
